f1d6a72b493c3a25c3139596cac07b5cf5c5f955
lvdelong <1526487591@qq.com>
2019-04-22 16:14:05 +0800

    手机验证码

---
+++ springcloud_api/src/main/java/com/jk/utils/CommonConst.java
@@ -0,0 +1,26 @@
+package com.jk.utils;
+
+public class CommonConst {
+
+	/**
+	 * 导航缓存key
+	 */
+		//用于短信验证的接口常量
+		public static final String ACCOUNT_SID="9822b34791f04d0ba642d6d3adbc3f87";
+		
+		//短信 模板id
+		public static final String TEMPLATE_ID="397723972";
+		
+		//授权码
+		public static final String AUTH_TOKEN="8af82dfb8a9442fc912fa010c3bf9a92";
+		//用于短信验证的接口常量
+//		public static final String ACCOUNT_SID="c5d091fac9b848a69d767b92508df161"; 
+		
+		//短信 模板id
+//		public static final String TEMPLATE_ID="396517847";
+		
+		//授权码
+//		public static final String AUTH_TOKEN="f213debaa98f45d2852c0a9b9636e3d4";
+
+
+}
